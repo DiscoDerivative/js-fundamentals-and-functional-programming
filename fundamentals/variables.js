7caf316a7d9f44da38fb12b5fc6af35b40bfa80e
disco <144652524+DiscoDerivative@users.noreply.github.com>
2023-12-18 23:45:42 -0500

    Create variables.js

---
+++ fundamentals/variables.js
@@ -0,0 +1,12 @@
+// If a variable will change use let
+let fruit = "Apple";
+fruit = "Banana";
+
+// You can use var as well
+var number = 3;
+
+// Const is if it will not change throughout the entire program
+const name = "Joe";
+
+//Different data types
+const skyBlue = true; // Boolean;
